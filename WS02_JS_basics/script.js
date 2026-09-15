b354e006a5afca536736465a6c282e070ac6e35b
Jani.Skogster <Jani.Skogster@student.laurea.fi>
2026-09-15 18:22:25 +0300

    .

--- WS02_JS_basics/script.js
+++ WS02_JS_basics/script.js
@@ -1,36 +1,3 @@
-const userName = "Jani";
-let age = 20;
-const favouriteAnimal = "Cat";
-
-console.log(userName);
-console.log(age);
-console.log(favouriteAnimal);
-
-console.log(
-  `Hello! My name is ${userName} and my favourite animal is the ${favouriteAnimal}.`
-);
-
-const name = prompt("What is your name?");
-
-console.log(`Hello ${name}! Welcome to JavaScript.`);
-
-const userAge = Number(prompt("How old are you?"));
-
-if (userAge >= 18) {
-  console.log("You are an adult.");
-} else {
-  console.log("You are under 18.");
-}
-
-function greetUser(name) {
-  console.log("Hello " + name + "!");
-}
-
-greetUser("John");
-greetUser("Juho");
-greetUser("Emma");
-greetUser("Alex");
-
 const button = document.getElementById("myButton");
 
 button.addEventListener("click", function () {

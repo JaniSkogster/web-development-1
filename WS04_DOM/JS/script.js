75a5ffe0cc78b127c5f1b4111c740438868eb379
Jani.Skogster <Jani.Skogster@student.laurea.fi>
2026-09-24 14:41:41 +0300

    .

--- WS04_DOM/JS/script.js
+++ WS04_DOM/JS/script.js
@@ -4,3 +4,12 @@ const taskOneHeading = document.querySelector("#taskOneHeading");
 changeHeadingButton.addEventListener("click", function () {
     taskOneHeading.textContent = "Muokattu otsikko!";
 });
+
+  
+const animalButton = document.querySelector("#animalButton");
+const animalTable = document.querySelector("#animalTable");
+
+animalButton.addEventListener("click", function() {
+    animalTable.hidden = !animalTable.hidden;
+    console.log("nappia painettu");
+});

e68baa8c6cc06293b8e76afde6c34f4aeb26774c
Jani.Skogster <Jani.Skogster@student.laurea.fi>
2026-09-24 15:04:17 +0300

    .

--- WS04_DOM/JS/script.js
+++ WS04_DOM/JS/script.js
@@ -13,3 +13,21 @@ animalButton.addEventListener("click", function() {
     animalTable.hidden = !animalTable.hidden;
     console.log("nappia painettu");
 });
+
+
+const animalSelect = document.querySelector("#animalSelect");
+const animalName = document.querySelector("#animalName");
+const animalImage = document.querySelector("#animalImage");
+const animalDescription = document.querySelector("#animalDescription");
+
+animalSelect.addEventListener("change", function() {
+    const selectedAnimal = animalSelect.value;
+    console.log("Selected Animal", selectedAnimal);
+
+    if (selectedAnimal === "tiger") {
+        animalName.textContent = "Tiikeri";
+        animalImage.src = "images/tiger.png";
+        animalImage.alt = "Tämä on Tiikeri";
+        animalDescription.textContent = "Tiikerit ovat raidallisia eläimiä";
+    }
+});
